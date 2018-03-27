6a1c46705f5cc6b68faa184621b8e8bde489d368
Assisneto <netodamasceno12@gmail.com>
2018-03-26 22:20:01 -0300

    first commit

---
+++ produtos.js
@@ -0,0 +1,1 @@
+console.log("listagem de produto");
---
+++ produtosWeb.js
@@ -0,0 +1,8 @@
+let http =  require('http');
+
+let server = http.createServer(req,res => {
+    res.end("<html><body><h1>Listando os produtos</h1></body></html>");
+});
+server.listen(3000);
+
+console.log("servidor rodando");
