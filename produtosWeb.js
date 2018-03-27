2d2c3958e3b203047819f9b2ba020c46d9c88a46
Assisneto <netodamasceno12@gmail.com>
2018-03-26 22:24:19 -0300

    arrow function in creatServer

--- produtosWeb.js
+++ produtosWeb.js
@@ -1,6 +1,6 @@
 let http =  require('http');
 
-let server = http.createServer(req,res => {
+let server = http.createServer((req, res) => {
     res.end("<html><body><h1>Listando os produtos</h1></body></html>");
 });
 server.listen(3000);
